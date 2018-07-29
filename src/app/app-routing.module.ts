b75b6b30f27af4bb2ccf28ef40e1f8e9830a6833
Connie <clai011@ucr.edu>
2018-07-29 03:36:36 -0700

    about comp routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-// home comp
+// components: home, current weather, forecast, about
 import { HomeComponent } from './home/home.component';
 import { CurrentComponent } from './current/current.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // path to home
+  { path: '', component: HomeComponent },
   { path: 'current', component: CurrentComponent },
   { path: 'forecast', component: ForecastComponent},
-  // about
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CurrentComponent } from './current/current.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CurrentComponent,
-    ForecastComponent
+    ForecastComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
